3c1f6297132aea60f5911fe54555a9ddd34ded06
Apon Ghosh <67550120+AponGhosh@users.noreply.github.com>
2024-06-01 01:13:02 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,15 @@ st.set_page_config(page_title="🦙 Llama 2 Chatbot 💬")
 # Replicate Credentials
 with st.sidebar:
     st.title('🦙 Llama 2 Chatbot 💬')
-    #st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        replicate_api = st.text_input('Enter Replicate API key:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+            st.warning('Please enter your Replicate API key!', icon='⚠️')
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+            st.success('Successful! Ask your questions ..., icon='✅')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     
